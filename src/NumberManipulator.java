131f0307fbfe3cc147c6316ec3a76563e3381944
Aleksander <144763744+AleksanderQA@users.noreply.github.com>
2024-02-08 09:27:01 +0200

    Update NumberManipulator.java

--- src/NumberManipulator.java
+++ src/NumberManipulator.java
@@ -1,15 +1,15 @@
 public class NumberManipulator {
 
-    int incrementByOne(int number) { // метод для перменной number
-        number++; // уменьшение значения переменной на -1
-        return number; // спросить на уроке
+    int incrementByOne(int number) {  
+        number++; 
+        return number;
 
 
     }
 
-    int decrementByOne (int number) { // метод для перменной number
-        number--; // увеличение значения  переменной на +1
-        return number; // спросить на уроке
+    int decrementByOne (int number) { 
+        number--; 
+        return number; 
 
     }
 }
